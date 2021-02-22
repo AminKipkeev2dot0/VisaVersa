92054e1a0c7cc1d92af577e134ddd4d9ced3c0bf
AminKipkeev2dot0 <aminkipkeev12@gmail.com>
2021-02-22 10:46:54 +0300

    Added an reversed text page downloading well

--- VisaVersa/VisaVersa/views.py
+++ VisaVersa/VisaVersa/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'main.html')
+
+
+def reversed(request):
+    user_text=request.GET['usertext']
+    reversedtext=user_text[::-1]
+    return render(request, 'reversedtext.html', {'usertext':user_text, "reversedtext":reversedtext})
